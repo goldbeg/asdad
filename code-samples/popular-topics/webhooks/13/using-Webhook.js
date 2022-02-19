abbc144ed6d9f6396e5e3e3502d9fac2260ea2ec
goldbeg <83942798+goldbeg@users.noreply.github.com>
2022-02-18 20:17:49 -0800

    Initial commit

---
+++ code-samples/popular-topics/webhooks/13/using-Webhook.js
@@ -0,0 +1,31 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const embed = new MessageEmbed()
+	.setTitle('Some Title')
+	.setColor('#0099ff');
+
+client.once('ready', async () => {
+	const channel = client.channels.get('222197033908436994');
+	try {
+		const webhooks = await channel.fetchWebhooks();
+		const webhook = webhooks.find(wh => wh.token);
+
+		if (!webhook) {
+			return console.log('No webhook was found that I can use!');
+		}
+
+		await webhook.send({
+			content: 'Webhook test',
+			username: 'some-username',
+			avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+			embeds: [embed],
+		});
+	} catch (error) {
+		console.error('Error trying to send: ', error);
+	}
+});
+
+client.login(token);
